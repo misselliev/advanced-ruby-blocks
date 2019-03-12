9827787e07116edb552ca29c0f0fb83b2f66f2d4
Elizabeth Villalejos <nekobanpaia@gmail.com>
2019-03-12 15:03:32 -0700

    starter files for bubble sort

---
+++ BubbleSort/bubble_sort.rb
@@ -0,0 +1,4 @@
+#   Build a method #bubble_sort that takes an array and returns a sorted array. It must use the bubble sort methodology
+
+# testing
+bubble_sort([4, 3, 78, 2, 0, 2]) # [0,2,2,3,4,78]
