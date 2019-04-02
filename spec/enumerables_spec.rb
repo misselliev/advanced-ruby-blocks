5660933d63594bb29df45e9aaa85554c33f25690
Elizabeth Villalejos <nekobanpaia@gmail.com>
2019-04-02 15:27:54 -0700

    adding feedback

--- spec/enumerables_spec.rb
+++ spec/enumerables_spec.rb
@@ -1,55 +1,58 @@
 RSpec.describe Enumerable do
+  let(:array) { [1, 2, 3] }
+  let(:items) { [0, 1, 2] }
+  let(:numbers) { [1, 2, 3, 7, 5] }
+  let(:example) { [9, 3, 2, 4, 7] }
+
   describe 'my_each' do
     it 'returns an array with the items + 1' do
-      expect([1, 2, 3].my_each{ |item| item + 1 }).to eql([1, 2, 3])
+      expect(array.my_each { |item| item + 1 }).to eql(array)
     end
   end
   describe 'my_select' do
     it 'returns an array with the items > 3' do
-      expect([1, 2, 3, 7, 5].my_select{ |item| item > 3 }).to eql([7, 5])
+      expect(numbers.my_select { |item| item > 3 }).to eql([7, 5])
     end
   end
   describe 'my_each_with_index' do
     it 'it returns items index' do
-      expect([0, 1, 2].my_each_with_index{|item, index| index}).to eql([0, 1, 2])
+      expect(items.my_each_with_index { |_item, index| index }).to eql(items)
     end
   end
 
   describe 'my_all' do
     it 'checks if all elements fulfill a condition or not' do
-      expect([1, 2, 3].my_all?{|item| item.is_a?(Integer)}).to eql(true)
+      expect(array.my_all? { |item| item.is_a?(Integer) }).to eql(true)
     end
   end
 
   describe 'my_any' do
     it 'checks if any of the elements fulfill a condition or not' do
-      expect([1, 'test', 3].my_any?{|item| item.is_a?(Integer)}).to eql(true)
+      expect([1, 'test', 3].my_any? { |item| item.is_a?(Integer) }).to eql(true)
     end
   end
 
   describe 'my_none' do
     it 'checks if none of the elements fulfill a condition or not' do
-      expect([1, 'test', 3].my_none?{|item| item.is_a?(Integer)}).to eql(false)
+      expect([1, 'test', 3].my_none? { |item| item.is_a?(Integer) }).to eql(false)
     end
   end
 
   describe 'my_count' do
     it 'checks counts how many of the elements fulfill a condition' do
-      expect(['alpha', 'beta', 'theta', 'omega', 'a', 'b', 'c'].my_count{|word| word.size > 1 }).to eql(4)
+      expect(%w[alpha beta theta omega a b c].my_count { |word| word.size > 1 }).to eql(4)
     end
   end
 
   describe 'my_map' do
     it 'checks counts how many of the elements fulfill a condition' do
-      expect([9, 3, 2, 4, 7].my_map{|item| item.even?}).to eql([false, false, true, true, false])
+      expect(example.my_map(&:even?)).to eql([false, false, true, true, false])
     end
   end
 
   describe 'my_inject' do
     it 'adds all numbers in an array to give out a single number' do
-      expect([9, 3, 2, 4, 7].my_inject{|total, n| total*n}).to eql(1512)
+      expect(example.my_inject { |total, n| total * n }).to eql(25)
     end
   end
-
 end
-
